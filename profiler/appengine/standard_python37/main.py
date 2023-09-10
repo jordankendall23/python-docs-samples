fa99cc349fe748d61a4c9f9fb48b2e1820bf5db1
jordankendall23 <S3858790@student.rmit.edu.au>
2023-09-10 10:58:58 +1000

    testing

--- profiler/appengine/standard_python37/main.py
+++ profiler/appengine/standard_python37/main.py
@@ -39,7 +39,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     """Return a friendly HTTP greeting."""
-    return "Hello World!"
+    return "This is a test"
 
 
 if __name__ == "__main__":
